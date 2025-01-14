2c15dc5a0b855d307247ff64c1e4d9ab2cbdb3de
Manuel Bueno <manuel.bueno@edreamsodigeo.com>
2025-01-14 21:28:23 +0100

    Genetic Algorithms (GA) p. 38

---
+++ genetic_algorithm_js.py
@@ -0,0 +1,41 @@
+# Datos del problema
+
+JOB_0 = [(0, 3), (1, 2), (2, 2)]
+JOB_1 = [(0, 2), (2, 1), (1, 4)]
+JOB_2 = [(1, 4), (2, 3)]
+
+jobs = [
+    JOB_0,
+    JOB_1,
+    JOB_2
+]
+
+
+
+# Inicializar población aleatoria
+def initialize_population(n_individuals, jobs):
+    pass
+
+
+def evaluate_fitness():
+    pass
+
+# Cruce por orden
+def crossover(ca, cb):
+    print(f"{ca} {cb}")
+
+# Mutación de individuos
+def mutate(ca, cb):
+    print(f"{ca} {cb}")
+
+def increase_population():
+    pass
+
+# Algoritmo genético
+def genetic_algorithm(jobs, params):
+    print(jobs)
+
+
+# Ejecutar el algoritmo
+genetic_algorithm(jobs, [])
+
