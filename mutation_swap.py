c13a19a2ae73c64b8fb4f206f980617b437c2e72
Manuel Bueno <manuel.bueno@edreamsodigeo.com>
2025-01-18 20:07:11 +0100

    Swap mutation respect precedence constraints

--- mutation_swap.py
+++ mutation_swap.py
@@ -9,4 +9,10 @@ class SwapMutation(Mutation):
         """Performs swap mutation."""
         if random.random() < self.mutation_rate:
             i, j = random.sample(range(len(schedule)), 2)
+            op_i = schedule[i]
+            op_j = schedule[j]
+            if op_i[0] == op_j[0]:
+                # If they belong to the same job, ensure no precedence is violated
+                if (op_i[1] > op_j[1] and i < j) or (op_i[1] < op_j[1] and i > j):
+                    return
             schedule[i], schedule[j] = schedule[j], schedule[i]
