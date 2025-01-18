e782568eb44cd81767bdc60ffbf24d6d81257cdb
Manuel Bueno <manuel.bueno@edreamsodigeo.com>
2025-01-18 20:18:59 +0100

    Scramble mutation respect precedence constraints

--- mutation_scramble.py
+++ mutation_scramble.py
@@ -8,6 +8,23 @@ class ScrambleMutation(Mutation):
     def mutation(self, schedule):
         i, j = sorted(random.sample(range(len(schedule)), 2))
         segment = schedule[i:j+1]
-        random.shuffle(segment)
-        schedule[i:j+1] = segment
+
+        # Group operations by job within the segment
+        job_groups = {}
+        for operation in segment:
+            job_id, op_index = operation
+            if job_id not in job_groups:
+                job_groups[job_id] = []
+            job_groups[job_id].append(operation)
+
+        # Create a list of job IDs to shuffle
+        job_ids = list(job_groups.keys())
+        random.shuffle(job_ids)
+
+        # Rebuild the segment in the shuffled job order
+        shuffled_segment = []
+        for job_id in job_ids:
+            shuffled_segment.extend(job_groups[job_id])
+
+        schedule[i:j+1] = shuffled_segment
         return schedule
